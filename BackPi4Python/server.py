f2906f5e3b5d659a58538e063836d5d60efa86e0
Vitor <77399148+VitorDAlbuquerque@users.noreply.github.com>
2025-05-21 22:53:11 -0300

    Add files via upload

--- BackPi4Python/server.py
+++ BackPi4Python/server.py
@@ -1,14 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from waitress import serve
-from Controller.bradescoInit import bradescoInit
-from Controller.bradescoFull import bradescoFull
+from Controller.listAll.listAllBradesco import listAllBradesco
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/list")
+@app.route("/bradesco")
 def list():
-    return jsonify(bradescoFull())
+    return jsonify(listAllBradesco())
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=3333)
