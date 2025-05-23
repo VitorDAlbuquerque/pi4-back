c3eac60979dce3747fb3d29e3a76a02fdf816045
Vitor <77399148+VitorDAlbuquerque@users.noreply.github.com>
2025-05-22 22:35:33 -0300

    Delete src directory

--- src/README.md
+++
@@ -1,2 +0,0 @@
-# pi4-back
-Semaismenos Back End
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import {app} from './app';
-
-const port = 3333
-
-app.listen(port, () => {
-    console.log(`Server started at http://localhost: ${port}`);
-})
-
